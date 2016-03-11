8d7bda0b3c941f5c349aaedb62be7b73d8d7e1d2
Nicolas Ligny <nicolas.ligny@ibrows.ch>
2016-03-11 15:46:57 +0100

    remove console.log

--- js/easyResponsiveTabs.js
+++ js/easyResponsiveTabs.js
@@ -170,7 +170,6 @@
 
                             $respTabs.find('.resp-tab-content[aria-labelledby = ' + $tabAria + '].' + options.tabidentify).slideDown().addClass('resp-tab-content-active');
                         } else {
-                            console.log('here');
                             $respTabs.find('.resp-tab-active.' + options.tabidentify).removeClass('resp-tab-active').css({
                                 'background-color': options.inactive_bg,
                                 'border-color': 'none'
